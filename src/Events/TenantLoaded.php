3d1172fbc7d6e81f95e12f262255b7b616cd74b0
Valerio <ilvalerione@gmail.com>
2018-01-17 19:58:16 +0100

    event not queue

--- src/Events/TenantLoaded.php
+++ src/Events/TenantLoaded.php
@@ -2,18 +2,8 @@
 
 namespace AventureCloud\MultiTenancy\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-
 class TenantLoaded
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
     /**
      * @var
      */
